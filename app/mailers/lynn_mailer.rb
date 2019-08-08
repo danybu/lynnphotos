223012fd0ffad401d504007fd7b7dfa283222800
Dany Buts <danybu.danybu@gmail.com>
2019-08-08 15:53:38 +0200

    setup google mail

--- app/mailers/lynn_mailer.rb
+++ app/mailers/lynn_mailer.rb
@@ -1,6 +1,6 @@
 class LynnMailer < ApplicationMailer
 
-$lynnMail = "danybu.danybu@gmail.com"
+$lynnMail = "dany.buts@gmail.com"
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
